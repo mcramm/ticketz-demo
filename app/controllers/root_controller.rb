631d4efa7950b495caaff9296a36aec31e49d86a
Michael Cramm <mike.cramm@clio.com>
2022-02-17 13:27:56 -0700

    simplify rendering of selected/unselected tickets

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,6 +2,7 @@ class RootController < ApplicationController
   def index
     @movies = Movie.all
     @showtimes = []
+    @selected_tickets = load_selected_tickets
   end
 
   def showtimes
@@ -33,6 +34,8 @@ class RootController < ApplicationController
     @ticket = Ticket.where(id: params.require(:id)).first!
     @ticket.update(status: :claimed_pending_payment)
 
+    @selected_tickets = load_selected_tickets
+
     respond_to do |format|
       format.turbo_stream
     end
@@ -44,8 +47,22 @@ class RootController < ApplicationController
     @ticket = Ticket.where(id: params.require(:id)).first!
     @ticket.update(status: :available)
 
+    @selected_tickets = load_selected_tickets
+
     respond_to do |format|
       format.turbo_stream
     end
+  end
+
+  private def load_selected_tickets
+    tickets = Ticket.claimed_pending_payment
+      .includes(:seat)
+      .includes(showtime: [:movie, :theatre])
+
+    tickets_by_movie = tickets.group_by { |ticket| ticket.showtime.movie.name }
+
+    tickets_by_movie.map do |movie_name, tickets|
+      [movie_name, tickets.group_by { |ticket| ticket.showtime.starts_at }]
+    end.to_h
   end
 end
